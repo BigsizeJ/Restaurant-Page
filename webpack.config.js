eaf3da4c2d1d489d21cd2f38b7891abd0394ebfb
BigsizeJ <apacjessie@gmail.com>
2022-06-25 11:28:29 +0800

    Starting the restaurant assignment

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: "development",
+    entry: './src/index.js',
+    devtool: 'inline-source-map',
+    output: {
+        filename: '[name].bundle.js',
+        assetModuleFilename: '[name][ext]',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html',
+        })
+    ],
+    devServer: {
+        port: 3000,
+        hot: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss/i,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(svg|png|jpg|jpg|gif)/i,
+                type: 'asset/resource'
+            },
+        ]
+    }
+}
